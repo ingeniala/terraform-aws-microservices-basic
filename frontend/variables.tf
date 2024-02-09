9a0248f771df8ee79552cf00289b9f2196c5ea19
Andres Kitaura <andres@ticketevolution.com>
2024-02-08 23:37:06 -0300

    WIP

--- frontend/variables.tf
+++ frontend/variables.tf
@@ -6,16 +6,19 @@ variable "tags_root" {
 variable "domain_name" {
   description = "Main domain name managed by AWS of the solution"
   type        = string
+  default     = "example.com"
 }
 
 variable "website_name" {
   description = "Name applied to the created website in S3"
   type        = string
+  default     = "example.com"
 }
 
 variable "website_subdomain" {
   description = "Frontend subdomain to configure in Route53 and CDN distribution"
   type        = string
+  default     = ""
 }
 
 variable "acm_certificate_arn" {
@@ -25,5 +28,7 @@ variable "acm_certificate_arn" {
 
 variable "waf_arn" {
   description = "The ARN of the WAF WebACL"
+  type        = string
+
 }
 
